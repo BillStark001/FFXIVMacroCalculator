53e265e438e6596829e5ecb2262ef2d0c6bf19a8
Toshiaki Subashiko <billstark001@gmail.com>
2022-03-15 01:27:13 +0900

    Start to migrate it to .NET for performance

---
+++ FfxivMacroCalculator/ProductionSystem/Skill.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FfxivMacroCalculator.ProductionSystem
+{
+    public enum SpecialEffect
+    {
+        None = 0, 
+        HQOnly = 1,
+        SuccTo100AfterObservation = 2,
+        Eff50pIfNotEnoughEndurance = 4, 
+        ForceTo18AfterProcessing = 8, 
+        DivInnerStatic = 16, 
+        OnlyWhenInnerStatic = 32, 
+        NotInFrugal = 64, 
+        OnlyFirst = 128, 
+        AddInnerStatic = 256,
+        ElementMarkApplied = 512, 
+        DQualityAdd20ByInnerStatic = 1024,
+        NoWorkTime = 2048, 
+    }
+
+    public class PersistentEffect
+    {
+
+    }
+
+    public partial class Skill
+    {
+        // fields
+        public string NameKey { get; private set; } = "null";
+        public int LeastLevel { get; private set; } = 1;
+        public int DProcess { get; private set; } = 0;
+        public int DQuality { get; private set; } = 0;
+        public double SuccessRate { get; private set; } = 1;
+        public int DForce { get; private set; } = 0;
+        public int DEndurance { get; private set; } = 0;
+        public int DInnerStatic { get; private set; } = 0;
+        public SpecialEffect SpecialEffect { get; private set; } = SpecialEffect.None;
+        
+    }
+}
---
+++ FfxivMacroCalculator/ProductionSystem/WorkState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FfxivMacroCalculator.ProductionSystem
+{
+    public enum QualityState
+    {
+        Low = 0, 
+        Normal = 1, 
+        High = 2, 
+        Highest = 3
+    }
+
+    public class WorkState
+    {
+        public int WorkTime = 0;
+        public int PocessRate = 0;
+        public int Quality = 0;
+        public int Endurance = 0;
+        public int ProductionForce = 0;
+        public int InnerStatic = 0;
+        public QualityState CurrentQuality = QualityState.Normal;
+        
+
+    }
+}
